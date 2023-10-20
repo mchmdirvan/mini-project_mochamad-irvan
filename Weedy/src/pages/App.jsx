9565bca41f52b6e328dfd6b4a7d1d9e677eaa8e5
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-20 13:23:56 +0700

    bug: section not appear

--- Weedy/src/pages/App.jsx
+++ Weedy/src/pages/App.jsx
@@ -5,27 +5,28 @@ import Button from "../components/button";
 export default function App() {
   return (
     <>
-      <div className="relative">
-        <header className="absolute flex items-center justify-center">
-          <img
-            src={HeroImage}
-            alt="Wedding Cover"
-            className="w-[100vw] h-[100vh] brightness-50 object-cover object-bottom"
-          />
-          <div className="absolute flex flex-col pb-60 items-center">
-            <h1 className="text-5xl font-bold font-[Outfit] text-white">
-              Make Your Wedding Unforgettable with{" "}
-              <span className="font-[Niconne] text-8xl font-normal">weedy</span>
-            </h1>
-            <p className="text-2xl font-[Outfit] text-white">
-              A Modern Way to Invite Your Guests to Your Wedding
-            </p>
-            <Button
-            label="let's go!" className="font-[Niconne] text-2xl"/>
-          </div>
-        </header>
-        <Navbar />
-      </div>
+      <Navbar />
+      <header className="relative flex justify-center">
+        <img
+          src={HeroImage}
+          alt="Wedding Cover"
+          className="absolute w-[100vw] h-[100vh] brightness-50 object-cover object-bottom"
+        />
+        <div className="absolute flex flex-col items-center mt-40">
+          <h1 className="text-5xl font-bold font-[Outfit] text-white">
+            Make Your Wedding Unforgettable with{" "}
+            <span className="font-[Niconne] text-8xl font-normal">weedy</span>
+          </h1>
+          <p className="text-2xl font-[Outfit] text-white">
+            A Modern Way to Invite Your Guests to Your Wedding
+          </p>
+          <Button label="let's go!" className="font-[Niconne] text-2xl" />
+        </div>
+        <section className="absolute">
+        <p>abcd</p>
+      </section>
+      </header>
+      
     </>
   );
 }
