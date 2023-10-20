b4c92fae32294cb9297dda54f364db1ab0489435
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-20 10:27:55 +0700

    feat: create hero section

--- Weedy/src/pages/App.jsx
+++ Weedy/src/pages/App.jsx
@@ -1,9 +1,28 @@
 import Navbar from "../components/navbar";
+import HeroImage from "../assets/hero-section.webp";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      <div className="relative">
+        <header className="absolute flex items-center justify-center">
+          <img
+            src={HeroImage}
+            alt="Wedding Cover"
+            className="w-[100vw] h-[100vh] brightness-50 object-cover object-bottom"
+          />
+          <div className="absolute flex flex-col pb-60 items-center">
+            <h1 className="text-5xl font-bold font-[Outfit] text-white">
+              Make Your Wedding Unforgettable with{" "}
+              <span className="font-[Niconne] text-8xl font-normal">weedy</span>
+            </h1>
+            <p className="text-2xl font-[Outfit] text-white">
+              A Modern Way to Invite Your Guests to Your Wedding
+            </p>
+          </div>
+        </header>
+        <Navbar />
+      </div>
     </>
   );
 }
