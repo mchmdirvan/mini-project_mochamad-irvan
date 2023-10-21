fd660b8f0490e269b84fce6d4611c3a1570b1581
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-21 14:02:14 +0700

    style: refactor button styling

--- Weedy/src/components/button.jsx
+++ Weedy/src/components/button.jsx
@@ -16,7 +16,7 @@ function Button({ type, id, label, ariaLabel, className }) {
   return (
     <div className="mt-5">
       <button
-        className={`${className} border px-10 py-1 rounded-full mt-5 text-[#F8F0E5] hover:ring-2 hover:bg-[#472A08] hover:border-[#472A08] hover:ring-white`}
+        className={`${className} border px-10 py-1 rounded-full mt-5 hover:ring-2 hover:bg-[#472A08] hover:border-[#472A08] hover:ring-white`}
         type={type}
         id={id}
         aria-label={ariaLabel}
