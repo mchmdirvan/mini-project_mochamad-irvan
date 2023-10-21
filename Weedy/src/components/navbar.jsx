7de5a055be4724ea9322813a5112510f2a57db2f
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-21 23:47:25 +0700

    style: medium responsive design

--- Weedy/src/components/navbar.jsx
+++ Weedy/src/components/navbar.jsx
@@ -35,7 +35,7 @@ export default function Navbar() {
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden md:hidden">
                   {/* Mobile menu button*/}
                   <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#472A08] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="absolute -inset-0.5" />
@@ -69,7 +69,7 @@ export default function Navbar() {
                     as="div"
                     className="relative ml-3 flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
                   >
-                    <div className="hidden sm:ml-6 sm:block">
+                    <div className="hidden md:ml-6 sm:block">
                       <ul className="flex space-x-4">
                         <li>
                           <a
@@ -138,7 +138,7 @@ export default function Navbar() {
               </div>
             </div>
 
-            <Disclosure.Panel className="sm:hidden">
+            <Disclosure.Panel className="sm:hidden md:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2">
                 <ul>
                   <li>
