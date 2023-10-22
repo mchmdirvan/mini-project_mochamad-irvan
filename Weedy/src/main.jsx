d53d78b38f49cfe1ec3e7551e6ee8f2e080baf6e
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-22 11:26:39 +0700

    feat: implement react router

--- Weedy/src/main.jsx
+++ Weedy/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 
-import App from './pages/App'
+import App from './routes/Index'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
