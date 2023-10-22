0b5cd99a5a88cf64b09e281c2e6841da2027c7d7
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-22 11:26:00 +0700

    feat: create router configuration

---
+++ Weedy/src/routes/Index.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import LandingPage from "../pages/LandingPage"
+import Login from "../pages/Login"
+
+function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <div>404</div>,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
