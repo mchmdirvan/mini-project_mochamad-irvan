a2df7458ae2c843582ce4e5b76b577a6514eeb72
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-23 13:00:51 +0700

    style: style form input when it's error

--- Weedy/src/components/Form.jsx
+++ Weedy/src/components/Form.jsx
@@ -35,7 +35,7 @@ function Input({
         <label className="mt-5">{label}</label>
         <input
           className={`${className} ${
-            error ? "" : " "
+            error ? "border-b border-[#C70039] text-[#C70039] placeholder:text-[#C70039]" : ""
           } mt-2 py-2 pl-2 text-[#472A08] transition duration-300 ease-in-out"`}
           type={type}
           name={name}
