9b34ddb5b0c4a579cc50cfd288a4eab742c5c51d
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-23 13:03:26 +0700

    feat: Implemented form handling, validation, and authentication in Login component

--- Weedy/src/pages/Login.jsx
+++ Weedy/src/pages/Login.jsx
@@ -1,14 +1,52 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import React from "react";
+import * as z from "zod";
 
+import { useToken } from "../utils/context/token-context";
 import { useTitle } from "../utils/hooks/customHooks";
+import { login } from "../utils/apis/auth/api";
 import LoginImage from "../assets/login.webp";
 import { Input } from "../components/Form";
 import Button from "../components/Button";
+import Swal from "../utils/swal";
 
 function Login() {
   useTitle("Sign In | Weedy");
+
+  const { changeToken } = useToken();
+  const navigate = useNavigate();
+
+  const schema = z.object({
+    username: z.string().min(1, { message: "Username is required" }),
+    password: z.string().min(1, { message: "Password is required" }),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  async function handleLogin(data) {
+    try {
+      const result = await login(data);
+      changeToken(JSON.stringify(result));
+      navigate("/dashboard");
+    } catch (error) {
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        showCancelButton: false,
+      });
+    }
+  }
+
   return (
     <>
       <div className="flex font-[Outfit]">
@@ -31,29 +69,42 @@ function Login() {
             </p>
           </div>
 
-          <div className="flex flex-col gap-10">
+          <form
+            className="flex flex-col gap-10"
+            onSubmit={handleSubmit(handleLogin)}
+          >
             <Input
+              register={register}
+              name="username"
+              type="text"
+              error={errors.username?.message}
               className="border-b border-[#472A08] text-[#472A08] placeholder:text-[#472A08] w-[28rem] placeholder:text-xl focus:outline-none"
               placeholder="Enter your Username"
             />
             <Input
+              register={register}
+              name="password"
+              type="password"
+              error={errors.password?.message}
               className="border-b border-[#472A08] text-[#472A08] placeholder:text-[#472A08] w-[28rem] placeholder:text-xl focus:outline-none"
               placeholder="Enter your Password"
             />
-          </div>
-
-          <div className="flex flex-col gap-2 items-center">
             <Button
               label="Sign in"
-              className="text-[#472A08] w-[28rem] border border-[#472A08] font-bold text-xl"
+              type="submit"
+              className="text-[#472A08] w-[28rem] border border-[#472A08] font-bold text-xl hover:text-white"
               id="btn-submit"
             />
-            <p>
+            <p className="self-center">
               Don't have an account yet?{" "}
               <a href="#" className="underline">
                 Sign Up
               </a>
             </p>
+          </form>
+
+          <div className="flex flex-col gap-2 items-center">
+            
           </div>
         </div>
       </div>
