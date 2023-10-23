d2cc3bd1517eb5b06a1a820f3c586c584450573d
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-23 11:50:35 +0700

    style: for mobile

--- Weedy/src/pages/NotFound.jsx
+++ Weedy/src/pages/NotFound.jsx
@@ -8,23 +8,23 @@ import Button from "../components/Button";
 export default function NotFound() {
   const navigate = useNavigate();
   return (
-    <div className="flex bg-[#F7EDE5] items-center">
-      <div className="w-1/2">
+    <div className="flex bg-[#F7EDE5] items-center h-[100vh]">
+      <div className="w-1/2 hidden lg:block">
         <img src={NotFoundImage} alt="" />
       </div>
-      <div className="flex flex-col gap-5 text-[#594545] mb-20">
-        <h1 className="font-bold text-5xl">This Page Does not Exist</h1>
-        <p className="font-normal text-2xl max-w-xl leading-8">
+      <div className="flex flex-col px-5 gap-5 text-[#594545] lg:mb-20">
+        <h1 className="font-bold text-2xl lg:text-5xl">This Page Does not Exist</h1>
+        <p className="font-normal lg:text-2xl max-w-xl leading-8">
           We are so happy that you want to join us on our special day, but this
           page is still under construction.
         </p>
-        <p className="font-normal text-2xl max-w-xl leading-8">
+        <p className="font-normal lg:text-2xl max-w-xl leading-8">
           We are busy preparing everything for our big day, and we hope you are
           too. Please check back soon and don’t forget to RSVP.
         </p>
         <Button
           label="Let’s build something amazing with us!"
-          className="font-bold border border-black hover:text-white"
+          className="font-bold text-sm px-7 border border-black hover:text-white lg:text-lg"
           onClick={() => navigate("/")}
         />
       </div>
