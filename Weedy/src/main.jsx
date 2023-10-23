1a1fcc5cb29b9cdc4d1e1f3ea53ff335a54974fe
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-23 13:06:14 +0700

    feat: Add TokenProvider to wrap the App component. The TokenProvider is responsible for managing token-related state and making it available to the rest of the application.

--- Weedy/src/main.jsx
+++ Weedy/src/main.jsx
@@ -1,11 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-import App from './routes/Index'
-import './styles/index.css'
+import { TokenProvider } from "./utils/context/token-context";
+import App from "./routes/Index";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <TokenProvider>
+      <App />
+    </TokenProvider>
+  </React.StrictMode>
+);
