29f5974e952778e4b5cb7ebbbf074b69f4af972d
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-25 11:24:00 +0700

    fix: sidebar mobile screen

--- Weedy/src/components/Sidebar.jsx
+++ Weedy/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
 import { useNavigate, Link } from "react-router-dom";
 import React, { useState } from "react";
 
@@ -11,13 +11,8 @@ import Button from "../components/Button";
 import Swal from "../utils/swal";
 
 export default function Sidebar() {
-  const [isDrawerOpen, setDrawerOpen] = useState(false);
   const { token, changeToken } = useToken();
   const navigate = useNavigate();
-
-  const toggleDrawer = () => {
-    setDrawerOpen(!isDrawerOpen);
-  };
 
   function handleLogout() {
     changeToken();
@@ -34,17 +29,6 @@ export default function Sidebar() {
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
-          <label
-            htmlFor="my-drawer-2"
-            className=" drawer-button lg:hidden self-end z-10"
-            onClick={toggleDrawer}
-          >
-            {isDrawerOpen ? (
-              <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-            ) : (
-              <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-            )}
-          </label>
         </div>
         <div className="drawer-side">
           <label
@@ -53,9 +37,9 @@ export default function Sidebar() {
             className="drawer-overlay"
           ></label>
 
-          <div className="menu p-4 w-80 min-h-full bg-[#F8F7FC] text-base-content ">
+          <div className="menu p-4 w-80 min-h-full bg-[#F8F7FC] ">
             {/* Sidebar content here */}
-            <div className="flex justify-center items-center ">
+            <div className="  flex justify-center items-center ">
               <img className=" h-20" src={Logo} alt="Your Company" />
               <Link
                 className=" text-black hover:text-[#594545] font-[Niconne] text-5xl"
