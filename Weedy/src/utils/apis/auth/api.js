f7b93530a4c8012a05404b8a8389fbd298e6b6c9
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-27 09:07:26 +0700

    refactor: api.js

--- Weedy/src/utils/apis/auth/api.js
+++ Weedy/src/utils/apis/auth/api.js
@@ -1,26 +1,43 @@
-export const login = async (data) => {
-  try {
+export const userLogin = async (data) => {
+  return new Promise((resolve, reject) => {
     const dummyUser = { username: "admin", password: "password123" };
 
-    if (
-      data.username === dummyUser.username &&
-      data.password === dummyUser.password
-    ) {
-      return data;
-    } else if (
-      data.username !== dummyUser.username &&
-      data.password === dummyUser.password
-    ) {
-      throw Error("Invalid username");
-    } else if (
-      data.username === dummyUser.username &&
-      data.password !== dummyUser.password
-    ) {
-      throw Error("Invalid password ");
-    } else {
-      throw Error("Invalid username or password ");
-    }
-  } catch (error) {
-    throw Error(error.message);
-  }
+    setTimeout(() => {
+      if (
+        data.username === dummyUser.username &&
+        data.password === dummyUser.password
+      ) {
+        resolve({ message: "Login Success", payload: data });
+      } else if (
+        data.username === dummyUser.username &&
+        data.password !== dummyUser.password
+      ) {
+        reject({ message: "Invalid password", payload: null });
+      } else if (
+        data.username !== dummyUser.username &&
+        data.password === dummyUser.password
+      ) {
+        reject({ message: "Invalid username", payload: null });
+      } else {
+        reject({ message: "Invalid username or password", payload: null });
+      }
+    }, 1000);
+  });
+};
+
+export const userRegister = async (data) => {
+  return new Promise((resolve, reject) => {
+    const dummyUser = { username: "admin", password: "password123" };
+
+    setTimeout(() => {
+      if (
+        data.username === dummyUser.username &&
+        data.password === dummyUser.password
+      ) {
+        resolve({ message: "Register Success", payload: data });
+      } else {
+        reject({ message: "Username already exist", payload: null });
+      }
+    }, 1000);
+  });
 };
