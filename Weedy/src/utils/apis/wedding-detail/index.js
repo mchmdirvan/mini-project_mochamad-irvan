d517c231fa9bfc6496b9fd7c1b55eae135e19870
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-27 08:30:33 +0700

    feat: create index js

---
+++ Weedy/src/utils/apis/wedding-detail/index.js
@@ -0,0 +1,4 @@
+import { getWedding, createWedding, getDetailWedding } from "./api";
+import { productSchema } from "./types";
+
+export { getWedding, createWedding, getDetailWedding, productSchema };
