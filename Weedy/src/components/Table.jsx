35b6a7bdc04642e43c4a295e73ed69983ae1c930
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-28 09:22:32 +0700

    feat: create review table component

---
+++ Weedy/src/components/Table.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Table({ formValues=[] }) {
+  const reviewData = [
+    { label: "Bride's Name", value: formValues.brideName },
+    { label: "Groom's Name", value: formValues.groomName },
+    { label: "Bride's Bio", value: formValues.brideBio },
+    { label: "Groom's Bio", value: formValues.groomBio },
+    { label: "Agreement Date", value: formValues.agreementDate },
+    { label: "Reception Date", value: formValues.receptionDate },
+    { label: "Agreement Place", value: formValues.agreementAddress },
+    { label: "Agreement Hall", value: formValues.agreementHall },
+    { label: "Agreement City", value: formValues.agreementCity },
+    { label: "Reception Place", value: formValues.receptionAddress },
+    { label: "Reception Hall", value: formValues.receptionHall },
+    { label: "Reception City", value: formValues.receptionCity },
+    { label: "Scripture Quotes", value: formValues.scriptureQuotes },
+  ];
+
+  return (
+    <table>
+      <tbody>
+        {reviewData.map((item) => (
+          <tr key={item.label}>
+            <td>{item.label}</td>
+            <td>{item.value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
