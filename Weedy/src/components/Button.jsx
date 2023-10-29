48b7c0c3cf2d1e678b1bb447ba3c1483a6038cd4
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-29 11:33:37 +0700

    refactor: deleting tag div

--- Weedy/src/components/Button.jsx
+++ Weedy/src/components/Button.jsx
@@ -15,20 +15,18 @@ import React from "react";
  * }}
  */
 
-function Button({ type, id, label, ariaLabel, className, onClick,disabled }) {
+function Button({ type, id, label, ariaLabel, className, onClick, disabled }) {
   return (
-    <div>
-      <button
-        className={`${className} border px-10 py-1 rounded-full mt-5 hover:ring-2 hover:bg-[#472A08] hover:border-[#472A08] hover:ring-white`}
-        type={type}
-        id={id}
-        aria-label={ariaLabel}
-        onClick={onClick}
-        disabled={disabled}
-      >
-        {label}
-      </button>
-    </div>
+    <button
+      className={`${className} border px-10 py-1 rounded-full mt-5 hover:ring-2 hover:bg-[#472A08] hover:border-[#472A08] hover:ring-white`}
+      type={type}
+      id={id}
+      aria-label={ariaLabel}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
   );
 }
 
