ff74c8ab47ead5e454d5f1f6371e0a18fa8c73ce
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-30 08:31:17 +0700

    feat: fetching data and consume in the page

--- Weedy/src/pages/ViewInvitation.jsx
+++ Weedy/src/pages/ViewInvitation.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 
-export default function ViewInvitation() {
+import { getWeddings } from "../utils/apis/weddings/api";
+import { useTitle } from "../utils/hooks/customHooks";
+import Swal from "../utils/swal";
+
+export default function ViewIntitation() {
+  const [weddings, setWeddings] = useState(null);
+  const title =
+    weddings === null
+      ? "The Wedding of"
+      : `The Wedding of ${weddings[0].brideName} & ${weddings[0].groomName}`;
+  useTitle(title);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const result = await getWeddings();
+      setWeddings(result);
+    } catch (error) {
+      Swal.fire({
+        title: "Error",
+        text: "Failed to fetch wedding data",
+        showCancelButton: false,
+      });
+    }
+  }
+
   return (
-    <div>ViewInvitation</div>
-  )
+    <div>
+      {weddings === null ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p>Username: {weddings[0].username}</p>
+        </div>
+      )}
+    </div>
+  );
 }
