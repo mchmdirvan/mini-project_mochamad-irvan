68634363d4673df001a50362b5b9d737e9684b23
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-11-04 09:21:40 +0700

    refactor: route to view invitation

--- Weedy/src/pages/dashboard/Dashboard.jsx
+++ Weedy/src/pages/dashboard/Dashboard.jsx
@@ -25,6 +25,7 @@ export default function Dashboard() {
 
   const user = getDataFromLocalStorage("user") || "";
   const userID = getDataFromLocalStorage("userID") || "";
+  const userTheme = getDataFromLocalStorage("userTheme") || "";
 
   return (
     <div className="drawer">
@@ -81,12 +82,12 @@ export default function Dashboard() {
                   src={DashboardView}
                   alt=""
                   className="rounded-xl h-[17rem] max-w-[7rem] object-cover object-left border border-black sm:max-w-[14rem] sm:h-[20rem] sm:object-center lg:max-w-[13rem] lg:h-[22rem] hover:border-blue-400 hover:scale-105 transition-all"
-                  onClick={() => navigate(`/invitation/${userID}?to/irvan`)}
+                  onClick={() => navigate(`/invitation/${userTheme}/${userID}?to/irvan`)}
                 />
                 <Button
                   label="View Invitation"
                   className="absolute bottom-[1rem] left-2  bg-white rounded-md border-black hover:text-white px-2 text-[0.7rem] lg:bottom-[2rem] lg:text-sm lg:px-10"
-                  onClick={() => navigate(`/invitation/${userID}?to/irvan`)}
+                  onClick={() => navigate(`/invitation/${userTheme}/${userID}?to/irvan`)}
                 />
               </div>
               <div className="flex flex-col gap-3">
