b96d9b05bb0abbfb652069e293bb67d2969f9aa1
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-12-01 20:46:26 +0700

    refactor: deleting schema retype password

--- Weedy/src/utils/apis/auth/types.js
+++ Weedy/src/utils/apis/auth/types.js
@@ -5,13 +5,7 @@ export const loginSchema = z.object({
   password: z.string().min(1, { message: "Password is required" }),
 });
 
-export const registerSchema = z
-  .object({
-    username: z.string().min(1, { message: "Username is required" }),
-    password: z.string().min(6, { message: "Password is required" }),
-    repassword: z.string().min(6, { message: "Retype password is required" }),
-  })
-  .refine((data) => data.password === data.repassword, {
-    message: "Passwords don't match",
-    path: ["repassword"],
-  });
+export const registerSchema = z.object({
+  username: z.string().min(1, { message: "Username is required" }),
+  password: z.string().min(6, { message: "Password is required" }),
+});
